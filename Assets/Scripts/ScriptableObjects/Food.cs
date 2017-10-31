b87dfe83782e02572bd6c1a48d1a3af8040ad0a8
smkmth <dandavidnoon@gmail.com>
2017-10-31 16:33:09 +0000

    Added more item types
    Now items can also be tools and food. more to come on this front i guess

---
+++ Assets/Scripts/ScriptableObjects/Food.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+[System.Serializable]
+[CreateAssetMenu( menuName = "Assets/Item/Food", order =3)]//creates a new editor assets/create menu called character data 
+public class Food : Item 
+{
+	public float Nutrition = 0;
+
+	public string specialEffect = "NewEffect";
+
+
+
+}
